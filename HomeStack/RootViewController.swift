9dea4be72c8f35bcc92efee3d4535347ddde1ca6
assadiandre <assadiandre@gmail.com>
2018-03-03 11:55:43 -0800

    added navigation UI

--- HomeStack/RootViewController.swift
+++ HomeStack/RootViewController.swift
@@ -1,90 +1,75 @@
 //
-//  RootViewController.swift
-//  HomeStack
+//  ViewController.swift
+//  SnapLike
 //
-//  Created by Felix Moses on 3/3/18.
-//  Copyright © 2018 Felix Moses. All rights reserved.
+//  Created by Kartikeya Saxena Jain on 10/21/15.
+//  Copyright © 2015 Kartikeya Saxena Jain. All rights reserved.
 //
 
 import UIKit
+import Material
 
-class RootViewController: UIViewController, UIPageViewControllerDelegate {
-
-    var pageViewController: UIPageViewController?
-
-
+class RootViewController: UIViewController, UIScrollViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var allClasses: UICollectionView!
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = allClasses.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath  )
+        
+        
+        let viewInCell = cell.viewWithTag(1)
+        viewInCell?.layer.cornerRadius = 5
+        //
+        //        let background = cell.viewWithTag(1)
+        //        background?.backgroundColor = Color.blue.lighten2
+        //
+        //        let title = cell.viewWithTag(2) as! UILabel
+        //        title.textColor = UIColor.white
+        //        title.text = "Class: \(indexPath.row)"
+        
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        return CGSize(width: 400, height: 100.0)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // Configure the page view controller and add it as a child view controller.
-        self.pageViewController = UIPageViewController(transitionStyle: .pageCurl, navigationOrientation: .horizontal, options: nil)
-        self.pageViewController!.delegate = self
-
-        let startingViewController: DataViewController = self.modelController.viewControllerAtIndex(0, storyboard: self.storyboard!)!
-        let viewControllers = [startingViewController]
-        self.pageViewController!.setViewControllers(viewControllers, direction: .forward, animated: false, completion: {done in })
-
-        self.pageViewController!.dataSource = self.modelController
-
-        self.addChildViewController(self.pageViewController!)
-        self.view.addSubview(self.pageViewController!.view)
-
-        // Set the page view controller's bounds using an inset rect so that self's view is visible around the edges of the pages.
-        var pageViewRect = self.view.bounds
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            pageViewRect = pageViewRect.insetBy(dx: 40.0, dy: 40.0)
-        }
-        self.pageViewController!.view.frame = pageViewRect
-
-        self.pageViewController!.didMove(toParentViewController: self)
+        
+        
+        allClasses.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
+        
+        
+        navigationController?.navigationBar.backgroundColor = Color.blue.lighten2
+        
+        let menu = IconButton(image: Icon.cm.menu)
+        menu.tintColor = Color.white
+        let plus = IconButton(image: Icon.cm.add)
+        plus.tintColor = Color.white
+        
+        navigationItem.leftViews = [menu]
+        navigationItem.rightViews = [plus]
+        navigationItem.titleLabel.text = "Classes"
+        navigationItem.titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        navigationItem.titleLabel.textColor = Color.white
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    var modelController: ModelController {
-        // Return the model controller object, creating it if necessary.
-        // In more complex implementations, the model controller may be passed to the view controller.
-        if _modelController == nil {
-            _modelController = ModelController()
-        }
-        return _modelController!
-    }
-
-    var _modelController: ModelController? = nil
-
-    // MARK: - UIPageViewController delegate methods
-
-    func pageViewController(_ pageViewController: UIPageViewController, spineLocationFor orientation: UIInterfaceOrientation) -> UIPageViewControllerSpineLocation {
-        if (orientation == .portrait) || (orientation == .portraitUpsideDown) || (UIDevice.current.userInterfaceIdiom == .phone) {
-            // In portrait orientation or on iPhone: Set the spine position to "min" and the page view controller's view controllers array to contain just one view controller. Setting the spine position to 'UIPageViewControllerSpineLocationMid' in landscape orientation sets the doubleSided property to true, so set it to false here.
-            let currentViewController = self.pageViewController!.viewControllers![0]
-            let viewControllers = [currentViewController]
-            self.pageViewController!.setViewControllers(viewControllers, direction: .forward, animated: true, completion: {done in })
-
-            self.pageViewController!.isDoubleSided = false
-            return .min
-        }
-
-        // In landscape orientation: Set set the spine location to "mid" and the page view controller's view controllers array to contain two view controllers. If the current page is even, set it to contain the current and next view controllers; if it is odd, set the array to contain the previous and current view controllers.
-        let currentViewController = self.pageViewController!.viewControllers![0] as! DataViewController
-        var viewControllers: [UIViewController]
-
-        let indexOfCurrentViewController = self.modelController.indexOfViewController(currentViewController)
-        if (indexOfCurrentViewController == 0) || (indexOfCurrentViewController % 2 == 0) {
-            let nextViewController = self.modelController.pageViewController(self.pageViewController!, viewControllerAfter: currentViewController)
-            viewControllers = [currentViewController, nextViewController!]
-        } else {
-            let previousViewController = self.modelController.pageViewController(self.pageViewController!, viewControllerBefore: currentViewController)
-            viewControllers = [previousViewController!, currentViewController]
-        }
-        self.pageViewController!.setViewControllers(viewControllers, direction: .forward, animated: true, completion: {done in })
-
-        return .mid
-    }
-
-
+    
+    
 }
 
--- Podfile
+++ Podfile
@@ -1,4 +1,6 @@
 target 'HomeStack' do
+	use_frameworks!
 	pod 'Firebase/Core'
-    pod 'Firebase/Messaging'
+   	pod 'Firebase/Messaging'
+	pod 'Material', '~> 2.13'
 end
